bbfe6175dc09859851719ae7cdd4747f7abe83de
Mimoalexer <MimoAlexer@gmail.com>
2025-05-26 21:34:34 +0200

    WIP for the CityPlayerGui

--- citysystem/src/main/java/com/mimo/CityGui/CityPlayerGui.java
+++ citysystem/src/main/java/com/mimo/CityGui/CityPlayerGui.java
@@ -1,22 +1,19 @@
 package com.mimo.CityGui;
 
-import com.mimo.City;
 import com.mimo.Gui.BasicInventoryGui;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class MainGui extends BasicInventoryGui {
-    public MainGui(Player player) {
-        String title = City.getCitybyPlayer(player).getName();
+public class CityPlayerGui extends BasicInventoryGui {
+    public CityPlayerGui(Player player, String title) {
         super(player, title);
     }
 
     @Override
     protected ItemStack[] items() {
         addItem(9, 3, Material.BARRIER);
-        addItem(2, 2, Material.PLAYER_HEAD);
         for(int i = 0; i < 8; i++) {
             addItem(i, 0, Material.GRAY_STAINED_GLASS_PANE);
             addItem(i, 6, Material.GRAY_STAINED_GLASS_PANE);
@@ -29,14 +26,12 @@ public class MainGui extends BasicInventoryGui {
         event.setCancelled(true);
         switch (event.getCurrentItem()){
             case ItemStack _ when isItemStackClicked(event, Material.BARRIER) -> {
-                event.getWhoClicked().closeInventory();
-            }
-            case ItemStack _ when isItemStackClicked(event, Material.PLAYER_HEAD) -> {
-                CityPlayerGui cityPlayerGui = new CityPlayerGui(player, "City Players");
-                cityPlayerGui.show();
+                MainGui mainGui = new MainGui(player);
+                mainGui.show();
             }
             case null -> {}
             default -> {
+                event.setCancelled(true);
                 return;
             }
         }
