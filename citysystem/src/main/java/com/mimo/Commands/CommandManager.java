a003b3dbcdda0e25dc51a6d15c8153c74673f366
Mimoalexer <MimoAlexer@gmail.com>
2025-05-28 22:20:47 +0200

    WIP Commands with cool Brigardier

---
+++ citysystem/src/main/java/com/mimo/Commands/CommandManager.java
@@ -0,0 +1,15 @@
+package com.mimo.Commands;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.FloatArgumentType;
+import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandManager {
+    public static void registerCommands() {
+        // Commands.literal("city").
+        // TODO: Implement the city command
+    }
+}
