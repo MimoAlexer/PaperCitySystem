91fe032425778b1033f7dadcd50f1c1a017679a1
Mimoalexer <MimoAlexer@gmail.com>
2025-06-24 18:17:20 +0200

    Implemented EnemyGui class to display defender allies in active wars

--- src/main/java/com/mimo/wargui/EnemyGui.java
+++ src/main/java/com/mimo/wargui/EnemyGui.java
@@ -1,4 +1,46 @@
 package com.mimo.wargui;
 
-public class EnemyGui {
+import com.mimo.City;
+import com.mimo.War;
+import com.mimo.api.gui.AbstractInventoryGui;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnemyGui extends AbstractInventoryGui {
+    public EnemyGui(Player player, Component title) {
+        super(player, title);
+    }
+
+    @Override
+    protected ItemStack[] items() {
+        List<ItemStack> results = new ArrayList<>();
+        for (War war : City.getCityByPlayer(player).getWars()) {
+            for (City ally : war.getDefenderAllies()) {
+                ItemStack item = new ItemStack(Material.PAPER);
+                ItemMeta meta = item.getItemMeta();
+                meta.displayName(Component.text(ally.getName()));
+                item.setItemMeta(meta);
+                results.add(item);
+            }
+        }
+        return results.toArray(new ItemStack[0]);
+    }
+
+    @Override
+    protected void clickCallback(InventoryClickEvent event) {
+
+    }
+
+    @Override
+    protected void inventoryCloseCallback(InventoryCloseEvent event) {
+
+    }
 }
